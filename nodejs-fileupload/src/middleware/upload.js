57becc1bf98eea5c213a89550814ca3aed216b40
imaqib101 <imaqib101@live.com>
2022-03-24 15:39:20 +0530

    added uuid package for file save on db

--- nodejs-fileupload/src/middleware/upload.js
+++ nodejs-fileupload/src/middleware/upload.js
@@ -2,21 +2,23 @@ const util = require("util");
 const multer = require("multer");
 const { GridFsStorage } = require("multer-gridfs-storage");
 const dbConfig = require("../config/db");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 let storage = new GridFsStorage({
   url: dbConfig.url + dbConfig.database,
   options: { useNewUrlParser: true, useUnifiedTopology: true },
   file: (req, file) => {
-    const match = ["file/pdf", "file/txt", "file/doc"];
+    const match = ["file/pdf", "file/txt", "file/doc", "file/jpeg"];
 
     if (match.indexOf(file.mimetype) === -1) {
-      const filename = `${Date.now()}-saved-${file.originalname}`;
+      const filename = `${uuidv4()}-original-${file.originalname}`;
       return filename;
     }
 
     return {
       bucketName: dbConfig.fileBucket,
-      filename: `${Date.now()}-saved-${file.originalname}`,
+      filename: `${uuidv4()}-original-${file.originalname}`,
     };
   },
 });
@@ -24,4 +26,14 @@ let storage = new GridFsStorage({
 let uploadFiles = multer({ storage: storage }).array("file", 10);
 // let uploadFiles = multer({ storage: storage }).single("file");
 let uploadFilesMiddleware = util.promisify(uploadFiles);
+
+//TODO: Write file as new file to fs and db
+
+fs.writeFile("../tests/test", "data", function (err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("The file was saved!");
+});
+
 module.exports = uploadFilesMiddleware;
